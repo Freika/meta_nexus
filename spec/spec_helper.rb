81d8827f9a868f87f2032e80be724723efa72120
Freika <iamfrey@gmail.com>
2015-06-24 18:42:13 +0600

    Enable vcr.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "codeclimate-test-reporter"
 require 'meta_nexus'
 require 'dotenv'
-# require 'support/vcr'
+require 'support/vcr'
 Dotenv.load
 CodeClimate::TestReporter.start
